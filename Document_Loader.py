ffb2b9df151c743501de7386b09f5cacf09e12d7
Mahindra Ram <2100080019ai.ds@gmail.com>
2026-02-07 22:10:40 +0530

    created the Document Loader

---
+++ Document_Loader.py
@@ -0,0 +1,13 @@
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+import os
+import langchain
+
+
+
+
+
+load_dotenv()
+
+print("langchain", langchain.__version__)
